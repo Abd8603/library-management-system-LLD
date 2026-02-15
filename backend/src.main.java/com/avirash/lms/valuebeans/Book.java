0ba34525bf020c5fb59f47e4e5a8bb12012c190f
Abd8603 <80663656+Abd8603@users.noreply.github.com>
2026-02-15 13:30:33 +0530

    Added book class

---
+++ backend/src.main.java/com/avirash/lms/valuebeans/Book.java
@@ -0,0 +1,67 @@
+package com.avirash.lms.valuebeans;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, String isbn, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    //used to find uniqueness based on ISBN
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+}
